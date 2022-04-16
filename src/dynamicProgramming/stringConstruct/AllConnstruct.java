0b1dc6a1213f5389d0acc7d1dc09a8a3e906bbfb
Nitin <nitin.chaturvedi@sap.com>
2022-04-16 16:46:47 +0530

    update package structure

--- src/dynamicProgramming/stringConstruct/AllConnstruct.java
+++ src/dynamicProgramming/stringConstruct/AllConnstruct.java
@@ -1,3 +1,5 @@
+package dynamicProgramming.stringConstruct;
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
--- src/dynamicProgramming/targetSum/BestSum.java
+++ src/dynamicProgramming/targetSum/BestSum.java
@@ -1,3 +1,5 @@
+package dynamicProgramming.targetSum;
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
